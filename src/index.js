cc30b8ec37e5a74075ce16891d0b5d298da06253
comicstrip12345 <sabanalers@gmail.com>
2022-04-21 01:07:47 +0800

    Add updated component imports

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
-import Services from "./Components/Services";
-import Live from "./Components/Live";
-import FAQs from "./Components/FAQs";
+import ServiceLayout from "./Components/ServiceLayout";
+import LiveLayout from "./Components/LiveLayout";
+import FAQsLayout from "./Components/FAQsLayout";
 import "./index.css"
 import Team from "./Components/Team";
 import ContactUs from "./Components/ContactUs";
@@ -17,14 +17,13 @@ const App = ()=> {
     return (
         <div>
             <Navbar/>
-                <Hero/>
-                <Services/>
-                <Live/>
-                <FAQs/>
-                <Team/>
-                <ContactUs/>
-                <Footer/>
-            {/* <Header/> */}
+            <Hero/>
+            <ServiceLayout/>
+            <LiveLayout/>
+            <FAQsLayout/>
+            <Team/>
+            <ContactUs/>
+            <Footer/>
         </div>
     )
 }
