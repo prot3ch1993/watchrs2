859c9bf970a4679b3ffcb47c5c68517b3c5edd3a
comicstrip12345 <sabanalers@gmail.com>
2022-04-22 12:08:26 +0800

    Added fixed-top navbar and onScroll Navbar Design

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = ()=> {
-    
+    const [colorChange, setColorchange] = useState(false);
+    const changeNavbarColor = () =>{
+        if(window.scrollY >= 80){
+            setColorchange(true);
+        }
+        else{
+            setColorchange(false);
+        }
+    };
+    window.addEventListener('scroll', changeNavbarColor);
     return(
         
-        <nav className="navbar">
+        <nav className={colorChange? "navbar fixed-top colorChange":"navbar"}>
             <div className="container">
                 <h1 className="logoMain"><span><a href="/#">watchr</a></span></h1>
                 <nav className="nav">
