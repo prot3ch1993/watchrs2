53296f92522077f1a59d92c8b3d547228039c397
comicstrip12345 <sabanalers@gmail.com>
2022-04-23 12:05:48 +0800

    Code Optimization
    - SASS Code Optimization
    - remove FAQ question: is it safe for kids?
    - HTML Code Optimization

--- src/Components/FAQsDetail.js
+++ src/Components/FAQsDetail.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const FAQsDetail = (props) => {
     return (
-        <p className='contentLink'>
+        <h2>
             <a data-bs-toggle="collapse" href={props.link} role="button" aria-expanded="true" aria-controls={props.controls}>
-                {props.p}
+                {props.h2}
             </a>
-        </p>
+        </h2>
     )
 }
 
