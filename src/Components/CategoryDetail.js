bac495be657fa78fa869b1b49e58baf885cb374d
Cleo Beverly Torniado <cleo.torniado@gmail.com>
2022-04-25 12:24:33 +0800

    fixed modal, separated movie array, fixed notfound

--- src/Components/CategoryDetail.js
+++ src/Components/CategoryDetail.js
@@ -1,77 +1,36 @@
 import React from 'react'
+import movies from './Movies'
 
-const movies = [
-  {
-    name: "Spiderman",
-    tempbg: "red",
-    desc: "no way home",
-    artist: "zendaya"
-  },
-
-  {
-    name: "Batman",
-    tempbg: "Black",
-    desc: "no way home",
-    artist: "zendaya"
-  },
-
-  {
-    name: "superman",
-    tempbg: "blue",
-    desc: "no way home",
-    artist: "zendaya"
-  },
-
-  {
-    name: "Spiderman",
-    tempbg: "red",
-    desc: "no way home",
-    artist: "zendaya"
-  },
-
-  {
-    name: "Batman",
-    tempbg: "Black",
-    desc: "no way home",
-    artist: "zendaya"
-  },
-
-  {
-    name: "superman",
-    tempbg: "blue",
-    desc: "no way home",
-    artist: "zendaya"
-  },
-
-]
 
 const CategoryDetail = (props) => {
   return (
     <>
-      <div className="container">
+      <div className="container p-4">
         <h2>{props.category}</h2>
 
         <div className="d-flex flex-col" style={{ overflowX: "scroll", maxWidth: "100vw" }}>
           {movies.map(
             (movie, index) => (
-              <div className="" style={{ backgroundColor: `${movie.tempbg} `, width: "600px", margin: "10px", height: "200px" }} key={index}>
-                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <div className="" style={{ backgroundColor: `${movie.tempbg} `, width: "600px", backgroundImage:`url(${movie.img})`, backgroundSize:"cover", margin: "10px", height: "300px" }} key={index} data-bs-toggle="modal" data-bs-target="#exampleModal">
+                 <button type="button" className="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 {movie.name}
                 </button>
 
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">{movie.name}</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-xl">
+                    <div className="modal-content">
+
+                        <div className="modal-header" style={{backgroundImage:`url(${movie.img})`,height:"40vh", objectFit:"cover", position:"relative"}}>
+                            <h2 className="modal-title p-3" id="exampleModalLabel" style={{position:"absolute",bottom:"0%"}}>{movie.name}</h2>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
+
+                        <div className="modal-body p-5">
                             {movie.desc}
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
     return(
@@ -8,7 +9,7 @@ const Hero = () => {
                     <div className="col-12 col-xl-6">
                         <div className="content">
                             <h1 className="hero"><span>Watch anytime you want while you’re at home.</span></h1>
-                            <button className="hero">Sign In</button>
+                            <button className="hero"><Link to="/loginform">Sign In</Link></button>
                             <button className="hero">Sign Up</button>
                         </div>
                     </div>
