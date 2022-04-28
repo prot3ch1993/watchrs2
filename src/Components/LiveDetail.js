5fa5cd3667edd97f244eb56eefaace2202299751
Cleo Beverly Torniado <cleo.torniado@gmail.com>
2022-04-28 18:46:22 +0800

    Added animations

--- src/Components/LiveDetail.js
+++ src/Components/LiveDetail.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 const LiveDetail = (props) => {
     return (
-        <div className='col-12 col-sm-4 col-md-12 col-lg-4 col-xl-4'>
-            <img src={props.img} alt="Live Images"/>
-            <h2>{props.h2}</h2>
-            <button className='live' data-bs-toggle="modal" data-bs-target="#exampleModal">
-                <p>{props.button}</p>
-            </button>
-        </div>
-        
+        <Fade down cascade>
+            <div className='col-12 col-sm-4 col-md-12 col-lg-4 col-xl-4'>
+                <img src={props.img} alt="Live Images" />
+                <h2>{props.h2}</h2>
+                <button className='live' data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    <p>{props.button}</p>
+                </button>
+            </div>
+        </Fade>
+
     )
 }
 
