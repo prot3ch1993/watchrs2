84b59a77b583ee99e5b57d7947fa641c8c933521
comicstrip12345 <sabanalers@gmail.com>
2022-04-29 11:56:33 +0800

    Added Log Out Alert

--- src/Components/LoggedOut.js
+++ src/Components/LoggedOut.js
@@ -1,19 +1,6 @@
-import React from 'react'
-// import { Outlet } from "react-router-dom";
-
 const LoggedOut = () => {
-
-
-
     return (
-        <div className='col-12 logOutInd'>
-            <div className="row" >
-
-
-                <h1 className="hero"><span>See you again!</span></h1>
-                <h1>You have logged out</h1>
-            </div>
-        </div >
+        alert("See you Again. You have Logged Out.")
     )
 }
 
